de1239a029addb2fa52853639072ceb15319e87a
oyvind1501 <oyvind1401@gmail.com>
2019-04-07 17:39:55 +0200

    Modify visulizer

--- scripts/magnetic_vector_visualizer.py
+++ scripts/magnetic_vector_visualizer.py
@@ -1,64 +1,66 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import MagneticField
-from visualization_msgs import Marker
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
 
 
 def callback(data):
-    global x_vector, y_vector, z_vector
+    visualization = Marker()
 
-    x_vector = data.magnetic_field.x
-    y_vector = data.magnetic_field.y
-    z_vector = data.magnetic_field.z
+    visualization.header.frame_id = "magnetometer_link"
+    visualization.header.stamp = rospy.Time()
+    visualization.ns = "my_namespace"
+    visualization.id = 0
+    visualization.type = visualization.ARROW
+    visualization.action = visualization.ADD
+    visualization.pose.position.x = 0
+    visualization.pose.position.y = 0
+    visualization.pose.position.z = 0
+
+    p1 = Point()
+    p2 = Point()
+
+    p1.x = 0
+    p1.y = 0
+    p1.z = 0
+    p2.x = data.magnetic_field.x
+    p2.y = data.magnetic_field.y
+    p2.z = data.magnetic_field.z
+
+    visualization.points.append(p1)
+    visualization.points.append(p2)
+
+    #visualization.points[0].x = 0
+    #visualization.points[0].y = 0
+    #visualization.points[0].z = 0
+    #visualization.points[1].x = x_vector
+    #visualization.points[1].y = y_vector
+    #visualization.points[1].z = z_vector
+
+    visualization.pose.orientation.x = 0
+    visualization.pose.orientation.y = 0
+    visualization.pose.orientation.z = 0
+    visualization.pose.orientation.w = 1
+    visualization.scale.x = 0.2
+    visualization.scale.y = 0.3
+    visualization.scale.z = 0.2
+
+    print("sdaf")
+
+    visualization_publisher.publish(visualization)
 
 
-def listen_for_magnetic_vector():
-    rospy.init_node('visualization_node')
+def publish_visualization_of_magnetic_vector():
+    global visualization_publisher
+    # Initialize node
+    rospy.init_node('magnetic_vector_visualization')
+    visualization_publisher = rospy.Publisher('em7180/vis', Marker, queue_size=10)
     rospy.Subscriber('imu/mag', MagneticField, callback)
     rospy.spin()
 
 
-def publish_visualization_of_magnetic_vector():
-    visualization_publisher = rospy.Publisher('em7180/vis', Marker, queue_size=10)
-
-    # Initialize node
-    rospy.init_node('magnetic_vector_visualization')
-
-    rate = rospy.Rate(10)
-
-    while not rospy.is_shutdown():
-        visualization = Marker()
-
-        visualization.header.frame_id = "magnetometer_link"
-        visualization.header.stamp = rospy.Time()
-        visualization.ns = "my_namespace"
-        visualization.id = 0
-        visualization.type = 0
-        visualization.action = 0
-        visualization.pose.position.x = 0
-        visualization.pose.position.y = 0
-        visualization.pose.position.z = 0
-        visualization.points[0].x = 0
-        visualization.points[0].y = 0
-        visualization.points[0].z = 0
-        visualization.points[1].x = x_vector
-        visualization.points[1].y = y_vector
-        visualization.points[1].z = z_vector
-        visualization.pose.orientation.x = 0
-        visualization.pose.orientation.y = 0
-        visualization.pose.orientation.z = 0
-        visualization.pose.orientation.w = 1
-        visualization.scale.x = 0.2
-        visualization.scale.y = 0.3
-        visualization.scale.z = 0.2
-
-        visualization_publisher.publish(visualization)
-
-        rate.sleep()
-
-
 if __name__ == '__main__':
-    listen_for_magnetic_vector()
     try:
         publish_visualization_of_magnetic_vector()
     except rospy.ROSinterruptException:
